371791e8cf37cbe84f8ee89a2238f04b1f37cc12
u2000144 <73668236+u2000144@users.noreply.github.com>
2021-02-18 18:22:28 -0600

    Dispute Audit changes and also changes for Cancel reason frop down from List of Values

--- HCAaudit.Service.Portal.AuditUI/ViewModel/EmailTemplate.cs
+++ HCAaudit.Service.Portal.AuditUI/ViewModel/EmailTemplate.cs
@@ -9,11 +9,9 @@ namespace HCAaudit.Service.Portal.AuditUI.ViewModel
     {
         public string  SendTo { get; set; }
         public string SendFrom { get; set; }
-
+        public string SendFromName { get; set; }
         public string ReplyTo { get; set; }
-
         public string Subject { get; set; }
         public string EmailBody { get; set; }
-
     }
 }
