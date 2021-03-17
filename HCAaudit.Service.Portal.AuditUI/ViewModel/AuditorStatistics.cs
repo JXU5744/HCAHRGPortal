27cc991768416b2de43c1fe9dcdc270f32953d31
Girish <girish.chavan@hcahealthcare.com>
2021-03-17 09:31:39 -0500

    Recent Audit Code Changes

--- HCAaudit.Service.Portal.AuditUI/ViewModel/AuditorStatistics.cs
+++ HCAaudit.Service.Portal.AuditUI/ViewModel/AuditorStatistics.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,10 @@ namespace HCAaudit.Service.Portal.AuditUI.ViewModel
 {
     public class AuditorStatistics
     {
-        public AuditorStatistics() {
+        public AuditorStatistics()
+        {
             RecentTicketLists = new List<RecentTicket>();
-            }
+        }
 
         public String YearToDate { get; set; }
         public String MonthToDate { get; set; }
@@ -21,7 +23,7 @@ namespace HCAaudit.Service.Portal.AuditUI.ViewModel
         public String TicketCode { get; set; }
         public String Agent34Id { get; set; }
         public String CompliantNonCompliant { get; set; }
-        public DateTime AuditDate { get; set; }
+        public string  AuditDate { get; set; }
         public String Dispute { get; set; }
     }
 }
